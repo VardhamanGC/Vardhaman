06478737e87c19e538bd2234c97f352577b7853d
VardhamanGC <vardhaman.cheruku@3framesailabs.com>
2024-10-24 17:27:30 +0530

    Add files via upload

---
+++ SpringBootService/src/test/java/com/example/SpringBootService/SpringBootServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
